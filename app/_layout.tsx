32b4be40e89913ae9f9ae0fcc3608650c0ea01ee
fredo09 <fred_15@outlook.com>
2025-07-12 21:31:40 -0600

    Eliminar archivos de pantalla y ajustar la estructura de navegación en la aplicación.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,9 +5,12 @@ import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
+import { useThemeColor } from '@/hooks/useThemeColor';
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
+  const backGroundColor = useThemeColor({}, 'background');
+
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   });
@@ -19,9 +22,20 @@ export default function RootLayout() {
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
+      <Stack
+        screenOptions={{
+          headerShadowVisible: false,
+          contentStyle: {
+            backgroundColor: backGroundColor
+          }
+        }}
+      >
+        <Stack.Screen
+          name= 'index'
+          options={{
+            title: 'Push App'
+          }}
+        />
       </Stack>
       <StatusBar style="auto" />
     </ThemeProvider>
