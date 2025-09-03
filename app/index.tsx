775e0e6189e8f1a5cb8d6e2602a07166b21881be
fredo09 <fred_15@outlook.com>
2025-09-02 22:42:59 -0600

    Agregar uso de hook para obtener el token de notificación push en la aplicación

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,17 @@
 import { ThemedText } from '@/components/ThemedText';
+import { usePushNotification } from '@/hooks/usePushNotification';
 import React from 'react';
 import { View } from 'react-native';
 
 const PushApp = () => {
-  return (
-	<View>
-	  <ThemedText>PushApp</ThemedText>
-	</View>
-  )
+	const { expoPushToken } = usePushNotification();
+
+	return (
+		<View style={{ marginHorizontal: 10, marginTop: 5 }}>
+			<ThemedText>Push Notification App</ThemedText>
+			<ThemedText>Expo Push Token: {expoPushToken}</ThemedText>
+		</View>
+	);
 }
 
 export default PushApp;
