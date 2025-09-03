16cb3d8513868af5dc8eb37bc09dfb1a71455d4a
fredo09 <fred_15@outlook.com>
2025-09-02 23:08:27 -0600

    Agregar manejo de notificaciones en el componente PushApp

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,43 @@
+import React from 'react';
+import { FlatList, View } from 'react-native';
+
 import { ThemedText } from '@/components/ThemedText';
 import { usePushNotification } from '@/hooks/usePushNotification';
-import React from 'react';
-import { View } from 'react-native';
 
 const PushApp = () => {
-	const { expoPushToken } = usePushNotification();
+	const { expoPushToken, notifications } = usePushNotification();
 
 	return (
 		<View style={{ marginHorizontal: 10, marginTop: 5 }}>
-			<ThemedText>Push Notification App</ThemedText>
-			<ThemedText>Expo Push Token: {expoPushToken}</ThemedText>
+			{/* <ThemedText>Push Notification App</ThemedText>
+			<ThemedText>Expo Push Token: {expoPushToken}</ThemedText> */}
+			<ThemedText 
+				style={{ marginTop: 10, fontWeight: 'bold', fontSize:25 }}>
+				Notifications
+			</ThemedText>
+
+			<FlatList
+				data={notifications}
+				keyExtractor={item => item?.request.identifier}
+				renderItem={({item}) => (
+					<View>
+						<ThemedText style={{fontWeight: 'bold'}}>
+							{item?.request?.content?.title}
+						</ThemedText>
+
+						<ThemedText>
+							{item?.request?.content?.body}
+						</ThemedText>
+
+						<ThemedText>
+							{JSON.stringify(item?.request?.content?.data, null, 2)}
+						</ThemedText>
+					</View>
+				)}
+				ItemSeparatorComponent={() => (
+					<View style={{ height: 1, backgroundColor: '#eee', opacity: 0.5 }}></View>
+				)}
+			/>
 		</View>
 	);
 }
